c9f70bf29779f8ef43034539921824ace834284f
Philip Haggard <philip.r.haggard@gmail.com>
2024-07-23 13:46:03 -0500

    EOS: 7/23/2024
    End of mid-day session for Tuesday July, 23

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const content = document.querySelector('.content');
 const container = document.querySelector('.container');
 
+/***** add the title *****/
 let title = document.createElement('div');
 title.classList.add('title');
 title.textContent = 'Welcome to Etch-a-Sketch!';
 content.appendChild(title);
 
+/***** add the change grid size button *****/
+let button = document.createElement('button');
+button.classList.add('button');
+button.textContent = 'Change the grid size!';
+/* button.addEventListener('click', getGridSize()); */
+content.appendChild(button);
+
+let gridSize = 16;
 let k = 1;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize; i++) {
 
     let rowContainer = document.createElement('div');
     rowContainer.classList.add(`row-${k}`);
@@ -16,11 +25,11 @@ for (let i = 0; i < 16; i++) {
 
     k++;
 
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < gridSize; j++) {
         let square = document.createElement('div');
         square.classList.add('square');
-        square.addEventListener('mouseover', function () { square.setAttribute('style', 'background-color:blue;'); }, false);
-        square.addEventListener('mouseout', function () { square.setAttribute('style', 'background-color:green;'); }, false);
+        square.addEventListener('mouseover', function () { square.setAttribute('style', 'background-color:blue;'); });
+        square.addEventListener('mouseout', function () { square.setAttribute('style', 'background-color:green;'); });
         rowContainer.appendChild(square);
     }
 }
