8be966a4d75c3c009967d0bea6e16fc21f4ea9cb
Philip Haggard <philip.r.haggard@gmail.com>
2024-07-23 13:02:33 -0500

    Finish basic grid layout

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+let k = 1;
+
+for (let i = 0; i < 16; i++) {
+
+    let rowContainer = document.createElement('div');
+    rowContainer.classList.add(`row-${k}`);
+    container.appendChild(rowContainer);
+
+    k++;
+
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        rowContainer.appendChild(square);
+    }
 }
